583eaea8d7660138f3841386e6b013c62a8350ab
Gulsenem <60161518+Gulsenem@users.noreply.github.com>
2022-02-17 10:10:33 +0100

    smallscreen

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ AOS.init();
 function hamburger()
 {
     document.getElementById("hamburger").style.display = "flex";
+    document.getElementById("hamburger_btn").style.display = "none";
+}
+function hamb_X()
+{
+    document.getElementById("hamburger").style.display = "none";
+    document.getElementById("hamburger_btn").style.display = "block"; 
 }
